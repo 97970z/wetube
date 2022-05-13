14f62bc1e003502636308002008bdad275ed3fdb
97970z <97970zz@gmail.com>
2022-05-13 18:33:32 +0900

    install mongoDB & model & async/await

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,16 @@
 import express from "express";
-import { see, edit, upload, deleteVideo } from "../controllers/videoController";
+import {
+  watch,
+  getEdit,
+  postEdit,
+  getUpload,
+  postUpload,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", see); // :id = url안에 변수를 포함시킬수있게 해줌 ( id라는 변수 사용 )
-videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.get("/:id(\\d+)", watch);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
--- src/server.js
+++ src/server.js
@@ -5,18 +5,14 @@ import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
-const PORT = 4000;
-
 const app = express(); // express() -> express application 생성
 app.use(morgan("dev"));
 
 app.set("view engine", "pug"); // view engine을 pug로 설정
 app.set("views", process.cwd() + "/src/views"); // pug의 현재작업디렉토리 기본값 변경
+app.use(express.urlencoded({ extended: true })); // express가 form의 value들을 이해할수 있게함
 app.use("/", globalRouter);
 app.use("/videos", videoRouter); // /videos로 시작하는 url로 접근하면 videoRouter안에 있는 url을 찾아줌
 app.use("/users", userRouter);
 
-const handleListening = () =>
-  console.log("Server listening on port http://localhost:4000");
-
-app.listen(PORT, handleListening);
+export default app;
