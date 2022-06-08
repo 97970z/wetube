7090d2d33aa4249c27578055aeab14ef740b679e
97970z <97970zz@gmail.com>
2022-06-08 12:40:57 +0900

    join & login using Session

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  avatarUrl: String,
+  socialOnly: { type: Boolean, default: false },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  name: { type: String, required: true },
+  location: String,
+});
+
+// 암호 해싱 ( one-way function이라 절대 되돌릴 수 없음 )
+// 문자열을 알아볼수 없게 바꿔서 DB에 저장시킴.
+userSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 5);
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 // express라는 패키지 node_modules에서 찾아서 express란 이름으로 import함
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express(); // express() -> express application 생성
 app.use(morgan("dev"));
@@ -11,7 +14,17 @@ app.use(morgan("dev"));
 app.set("view engine", "pug"); // view engine을 pug로 설정
 app.set("views", process.cwd() + "/src/views"); // pug의 현재작업디렉토리 기본값 변경
 app.use(express.urlencoded({ extended: true })); // express가 form의 value들을 이해할수 있게함
-app.use("/", globalRouter);
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+  })
+);
+
+app.use(localsMiddleware);
+app.use("/", rootRouter);
 app.use("/videos", videoRouter); // /videos로 시작하는 url로 접근하면 videoRouter안에 있는 url을 찾아줌
 app.use("/users", userRouter);
 
