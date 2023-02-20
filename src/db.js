844af6c1ca65ed5f66fe124c60761f47adf04758
97970z <97970zz@gmail.com>
2023-02-20 14:10:48 +0900

    modify mongodb

--- src/db.js
+++ src/db.js
@@ -9,3 +9,13 @@ const handleError = (error) => console.log("❌ DB Error", error);
 
 db.on("error", handleError);
 db.once("open", handleOpen);
+
+// mongoose.connect(process.env.DB_URL);
+
+// const db = mongoose.connection;
+
+// const handleOpen = () => console.log("✅ Connected to DB");
+// const handleError = (error) => console.log("❌ DB Error", error);
+
+// db.on("error", handleError);
+// db.once("open", handleOpen);
